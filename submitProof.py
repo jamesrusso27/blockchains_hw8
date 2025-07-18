7faa00f6a1f7532412d21674ab5c7b14ac3ce818
jamesrusso27 <jamesrusso27@users.noreply.github.com>
2025-07-18 14:46:41 -0400

    finish assignment

--- submitProof.py
+++ submitProof.py
@@ -4,128 +4,81 @@ import string
 import json
 from pathlib import Path
 from web3 import Web3
-from web3.middleware import ExtraDataToPOAMiddleware  # Necessary for POA chains
-
+from web3.middleware import ExtraDataToPOAMiddleware
+from eth_account.messages import encode_defunct
+from sympy import primerange
 
 def merkle_assignment():
-    """
-        The only modifications you need to make to this method are to assign
-        your "random_leaf_index" and uncomment the last line when you are
-        ready to attempt to claim a prime. You will need to complete the
-        methods called by this method to generate the proof.
-    """
-    # Generate the list of primes as integers
     num_of_primes = 8192
     primes = generate_primes(num_of_primes)
-
-    # Create a version of the list of primes in bytes32 format
     leaves = convert_leaves(primes)
-
-    # Build a Merkle tree using the bytes32 leaves as the Merkle tree's leaves
     tree = build_merkle(leaves)
-
-    # Select a random leaf and create a proof for that leaf
-    random_leaf_index = 0 #TODO generate a random index from primes to claim (0 is already claimed)
-    proof = prove_merkle(tree, random_leaf_index)
-
-    # This is the same way the grader generates a challenge for sign_challenge()
-    challenge = ''.join(random.choice(string.ascii_letters) for i in range(32))
-    # Sign the challenge to prove to the grader you hold the account
-    addr, sig = sign_challenge(challenge)
-
-    if sign_challenge_verify(challenge, addr, sig):
-        tx_hash = '0x'
-        # TODO, when you are ready to attempt to claim a prime (and pay gas fees),
-        #  complete this method and run your code with the following line un-commented
-        # tx_hash = send_signed_msg(proof, leaves[random_leaf_index])
-
+    while True:
+        random_leaf_index = random.randint(0, num_of_primes - 1)
+        proof = prove_merkle(tree, random_leaf_index)
+        challenge = ''.join(random.choice(string.ascii_letters) for _ in range(32))
+        addr, sig = sign_challenge(challenge)
+        if sign_challenge_verify(challenge, addr, sig):
+            send_signed_msg(proof, leaves[random_leaf_index])
+            break
 
 def generate_primes(num_primes):
-    """
-        Function to generate the first 'num_primes' prime numbers
-        returns list (with length n) of primes (as ints) in ascending order
-    """
-    primes_list = []
-
-    #TODO YOUR CODE HERE
-
-    return primes_list
-
+    primes = []
+    for p in primerange(2, 90000):
+        primes.append(p)
+        if len(primes) == num_primes:
+            break
+    return primes
 
 def convert_leaves(primes_list):
-    """
-        Converts the leaves (primes_list) to bytes32 format
-        returns list of primes where list entries are bytes32 encodings of primes_list entries
-    """
-
-    # TODO YOUR CODE HERE
-
-    return []
-
+    return [int.to_bytes(p, 32, 'big') for p in primes_list]
 
 def build_merkle(leaves):
-    """
-        Function to build a Merkle Tree from the list of prime numbers in bytes32 format
-        Returns the Merkle tree (tree) as a list where tree[0] is the list of leaves,
-        tree[1] is the parent hashes, and so on until tree[n] which is the root hash
-        the root hash produced by the "hash_pair" helper function
-    """
-
-    #TODO YOUR CODE HERE
-    tree = []
-
+    tree = [leaves]
+    level = leaves
+    while len(level) > 1:
+        next_level = []
+        for i in range(0, len(level), 2):
+            a = level[i]
+            b = level[i+1] if i+1 < len(level) else level[i]
+            next_level.append(hash_pair(a, b))
+        tree.append(next_level)
+        level = next_level
     return tree
 
-
 def prove_merkle(merkle_tree, random_indx):
-    """
-        Takes a random_index to create a proof of inclusion for and a complete Merkle tree
-        as a list of lists where index 0 is the list of leaves, index 1 is the list of
-        parent hash values, up to index -1 which is the list of the root hash.
-        returns a proof of inclusion as list of values
-    """
-    merkle_proof = []
-    # TODO YOUR CODE HERE
-
-    return merkle_proof
-
+    proof = []
+    idx = random_indx
+    for level in merkle_tree[:-1]:
+        pair_idx = idx ^ 1
+        if pair_idx < len(level):
+            proof.append(level[pair_idx])
+        idx //= 2
+    return proof
 
 def sign_challenge(challenge):
-    """
-        Takes a challenge (string)
-        Returns address, sig
-        where address is an ethereum address and sig is a signature (in hex)
-        This method is to allow the auto-grader to verify that you have
-        claimed a prime
-    """
     acct = get_account()
-
     addr = acct.address
-    eth_sk = acct.key
-
-    # TODO YOUR CODE HERE
-    eth_sig_obj = 'placeholder'
-
+    eth_encoded_msg = encode_defunct(text=challenge)
+    eth_sig_obj = acct.sign_message(eth_encoded_msg)
     return addr, eth_sig_obj.signature.hex()
 
-
 def send_signed_msg(proof, random_leaf):
-    """
-        Takes a Merkle proof of a leaf, and that leaf (in bytes32 format)
-        builds signs and sends a transaction claiming that leaf (prime)
-        on the contract
-    """
     chain = 'bsc'
-
     acct = get_account()
     address, abi = get_contract_info(chain)
     w3 = connect_to(chain)
-
-    # TODO YOUR CODE HERE
-    tx_hash = 'placeholder'
-
-    return tx_hash
-
+    contract = w3.eth.contract(address=address, abi=abi)
+    nonce = w3.eth.get_transaction_count(acct.address)
+    tx = contract.functions.submit(random_leaf, proof).build_transaction({
+        'from': acct.address,
+        'nonce': nonce,
+        'gas': 500000,
+        'gasPrice': w3.toWei('10', 'gwei')
+    })
+    signed_tx = w3.eth.account.sign_transaction(tx, acct.key)
+    tx_hash = w3.eth.send_raw_transaction(signed_tx.rawTransaction)
+    return w3.toHex(tx_hash)
 
 # Helper functions that do not need to be modified
 def connect_to(chain):
@@ -179,7 +132,7 @@ def sign_challenge_verify(challenge, addr, sig):
         Helper to verify signatures, verifies sign_challenge(challenge)
         the same way the grader will. No changes are needed for this method
     """
-    eth_encoded_msg = eth_account.messages.encode_defunct(text=challenge)
+    eth_encoded_msg = encode_defunct(text=challenge)
 
     if eth_account.Account.recover_message(eth_encoded_msg, signature=sig) == addr:
         print(f"Success: signed the challenge {challenge} using address {addr}!")
